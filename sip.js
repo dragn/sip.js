06c2d57b5cb454806e973f35b721716ccddd52fb
Daniel Sabelnikov <dsabelnikov@gmail.com>
2016-03-29 11:22:16 +0300

    Check auth header format

--- sip.js
+++ sip.js
@@ -118,14 +118,14 @@ function parseCSeq(d) {
 function parseAuthHeader(d) {
   var r1 = applyRegex(/([^\s]*)\s+/g, d);
   if (!r1) {
-    console.log('Invalid format for auth header: ' + d);
+    console.log('Invalid format for auth header: ', d);
     return {};
   }
   var a = {scheme: r1[1]};
 
   var r2 = applyRegex(/([^\s,"=]*)\s*=\s*([^\s,"]+|"[^"\\]*(?:\\.[^"\\]*)*")\s*/g, d);
   if (!r2) {
-    console.log('Invalid format for auth header: ' + d);
+    console.log('Invalid format for auth header: ', d);
     return a;
   }
   a[r2[1]]=r2[2];
